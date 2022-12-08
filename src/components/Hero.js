428ed6ce7c14937fa4ac74c5c46e27227d252e4a
nxelia99 <nxelia99@gmail.com>
2022-12-08 12:51:05 +0100

    second commit

--- src/components/Hero.js
+++ src/components/Hero.js
@@ -1,25 +1,27 @@
 import React from 'react';
 import Container from 'react-bootstrap/Container';
 import Button from 'react-bootstrap/Button';
+import MyNavbar from './MyNavbar';
 
 
 const Hero = () => {
   return (
-    <Container>
-			<video src="./media/hero.mp4" autoPlay muted playsinline loop />
-			<Container className='container'>
-				<h1>Your data is secure with us</h1>
-				<h1>
-					We provide the best security systems for clients all over the world
-				</h1>
-				<>
-					<a href='singup'>
-						<Button>Get Started</Button>
-					</a>
-					<Button>Find More</Button>
-				</>
-			</Container>
-		</Container>
+    <>
+                <video src="./media/hero.mp4" autoPlay muted playsinline loop />
+                <MyNavbar />
+                <Container className='container'>
+                    <h1>Your data is secure with us</h1>
+                    <h1>
+                        We provide the best security systems for clients all over the world
+                    </h1>
+                    <>
+                        <a href='singup'>
+                            <Button>Get Started</Button>
+                        </a>
+                        <Button>Find More</Button>
+                    </>
+                </Container>
+        </>
   )
 }
 
---
+++ src/components/MyNavbar.js
@@ -0,0 +1,12 @@
+import React from 'react'
+import logo from '../media/logo.png'
+
+const MyNavbar = () => {
+  return (
+    <div className='MyNavbar my-5'>
+      <img src={logo} alt={'logo'} width={90} />
+    </div>
+  )
+}
+
+export default MyNavbar
